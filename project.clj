4512f2d823b4fcee6f0532f3bf95e968574cb6c9
Steven Deobald <steven@deobald.ca>
2020-09-17 17:42:21 +0530

    Fixing license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject kosa-crux "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "The Pariyatti Library"
+  :url "http://github.com/pariyatti/kosa-crux"
+  :license {:name "GNU Affero General Public License 3.0"
+            :url "https://www.gnu.org/licenses/agpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [mount "0.1.16"]
                  [aero "1.1.6"]
