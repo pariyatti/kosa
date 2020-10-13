8d7e4ba712011ae7893fab03db5474cc3c214bd3
Steven Deobald <steven@deobald.ca>
2020-10-13 13:20:22 +0530

    slf4j / logback in project.clj
    - fixed dependency conflicts with explicit versions for transients
    - bumped a couple dependency versions to the latest stable
    - depend on slf4j and both logback libs directly
    - add explicit java property for clojure.tools.logging
    
    super helpful links:
    
    https://lambdaisland.com/blog/2020-06-12-logging-in-clojure-making-sense-of-the-mess
    https://github.com/stuartsierra/log.dev/blob/master/project.clj

--- project.clj
+++ project.clj
@@ -4,25 +4,30 @@
   :license {:name "GNU Affero General Public License 3.0"
             :url  "https://www.gnu.org/licenses/agpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/tools.cli "1.0.194"]
                  [org.clojure/data.json "1.0.0"]
+                 [org.clojure/spec.alpha "0.2.187"]
+                 [org.clojure/tools.cli "1.0.194"]
                  [org.clojure/tools.logging "1.1.0"]
+                 ;; logging:
+                 [ch.qos.logback/logback-classic "1.2.3"]
+                 [ch.qos.logback/logback-core "1.2.3"]
+                 [org.slf4j/slf4j-api "1.7.30"]
                  ;; system:
-                 [ch.qos.logback/logback-classic "1.1.3"]
                  [mount "0.1.16"]
                  [tolitius/mount-up "0.1.3"]
                  [aero "1.1.6"]
                  ;; web:
-                 [ring/ring-core "1.7.1"]
+                 [ring/ring-core "1.8.2"]
                  [ring/ring-jetty-adapter "1.6.3"]
                  [ring/ring-json "0.5.0"]
                  [ring-logger "1.0.1"]
                  [metosin/reitit "0.5.6"]
                  [metosin/muuntaja "0.6.7"]
+                 [metosin/jsonista "0.2.7"] ;; force version: reitit / muuntaja dep
                  [hiccup "1.0.5"]
                  ;; data:
-                 [juxt/crux-core "20.09-1.12.1-beta" :exclusions [org.slf4j/slf4j-api]]
-                 [juxt/crux-rocksdb "20.09-1.12.1-beta" :exclusions [org.slf4j/slf4j-api]]]
+                 [juxt/crux-core "20.09-1.12.1-beta"]
+                 [juxt/crux-rocksdb "20.09-1.12.1-beta"]]
   :plugins [[lein-scss "0.3.0"]]
 
   :scss {:builds
@@ -41,5 +46,6 @@
 
   :repl-options {:init-ns dev.repl}
   :profiles {:uberjar {:aot      :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"
+                                  "-Dclojure.tools.logging.factory=clojure.tools.logging.impl/slf4j-factory"]}
              :dev     {:dependencies [[org.clojure/test.check "1.1.0"]]}})
