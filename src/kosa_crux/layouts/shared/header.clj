e3b4dae26f0a5782106f02e9dba2ae834d9c9398
Steven Deobald <steven@solasa.gateway.sasktel.net>
2020-12-14 16:27:13 -0600

    Finished cleaning up site header
    - removed the :hover menu dropdown
    - cleaned up the styling so it's visibly symmetrical when in dark
      mode (Dark Reader, for example)
    - added a new button style for the main menu

--- src/kosa_crux/layouts/shared/header.clj
+++ src/kosa_crux/layouts/shared/header.clj
@@ -9,27 +9,20 @@
      [:a {:href home-url}
       [:img {:src "/images/pariyatti-logo-256-solid.png"
              :width "40"
-             :height "40"}]
+             :height "40"}]]
+     [:a {:href home-url}
       [:span title]]]
 
     [:div {:class "header-user"}
      [:p {:class "user-email"}
       [:a {:href other-home-url} (str "Switch to " other-title)]
       [:span "&nbsp; | &nbsp;"]
-      [:div.dropdown
-       [:span [:a.dropdown {:href "#"} "zig@pariyatti.org"]]
-       [:div.dropdown-content
-        [:div "TODO: Settings"]
-        [:div "TODO: Change Password"]
-        [:div "TODO: Login/Logout"]]]
-
       [:div.menu-wrapper
        [:input {:id "menu-check" :class "menu"
                 :type "checkbox" :name "menu"}]
-       [:label {:class "menu" :for "menu-check"}
-        "Menu"]
+       [:label {:class "menu btn btn-secondary" :for "menu-check"}
+        "zig@pariyatti.org"]
        [:ul {:class "submenu"}
-        [:li [:a {:href "#"} "Item 1"]]
-        [:li [:a {:href "#"} "Item 2"]]]]
-
-      ]]]))
+        [:li [:a {:href "#"} "TODO: Settings"]]
+        [:li [:a {:href "#"} "TODO: Change Passwored"]]
+        [:li [:a {:href "#"} "TODO: Login/Logout"]]]]]]]))
