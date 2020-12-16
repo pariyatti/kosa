28215e26744356f5fe1de17fd6a5d2432f9d4b45
Steven Deobald <steven@solasa.gateway.sasktel.net>
2020-12-16 17:38:13 -0600

    Adding some sundry functions (only, find-all-nested)
    - I didn't end up using these for the route lookup stuff, but these
      are both very useful functions I assume we'll need in the near
      future. Also, we might as well have a namespace to collect sundry fns
      sooner than later.

---
+++ src/kosa_crux/sundry.clj
@@ -0,0 +1,12 @@
+(ns kosa-crux.sundry)
+
+(defn find-all-nested
+  [m k]
+  (->> (tree-seq map? vals m)
+       (filter map?)
+       (keep k)))
+
+(defn only [coll]
+  (if (= 1 (count coll))
+    (first coll)
+    (throw (Exception. (format "Collection '%s' does not contain exactly one item." coll)))))
