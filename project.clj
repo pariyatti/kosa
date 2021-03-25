3378842daf20219e436b4604fd6dd86e7cc1a8a8
Steven Deobald <steven@deobald.ca>
2021-03-25 17:16:34 -0600

    A slightly saner dev logging config
    - got rid of the huge thread name and junk
    - added a draft RollingFileAppender for prod

--- project.clj
+++ project.clj
@@ -95,7 +95,8 @@
   :profiles {:uberjar {:aot      :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"
                                   "-Dclojure.tools.logging.factory=clojure.tools.logging.impl/slf4j-factory"]}
-             :dev     {:dependencies [[org.clojure/test.check "1.1.0"]]}}
+             :dev     {:dependencies [[org.clojure/test.check "1.1.0"]]
+                       :resource-paths ["config/dev"]}}
 
   :test-selectors {:default     (complement :integration)
                    :integration :integration
