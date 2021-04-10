ce1e7ccbb77cbc78ba1d2d13da36ef78256e4a3b
Steven Deobald <steven@deobald.ca>
2021-04-10 17:39:55 -0600

    Fixing failing test
    - not sure why file.io and bb/fs play nicely in emacs but not a cmd
      line repl

--- test/kosa/fixtures/file_fixtures.clj
+++ test/kosa/fixtures/file_fixtures.clj
@@ -1,6 +1,5 @@
 (ns kosa.fixtures.file-fixtures
-  (:require [clojure.java.io :as io]
-            [babashka.fs :as fs]))
+  (:require [babashka.fs :as fs]))
 
 (defn file [filename]
-  (io/file (fs/path "test/kosa/fixtures/files" filename)))
+  (fs/file (fs/path "test/kosa/fixtures/files" filename)))
