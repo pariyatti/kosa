753d451669a507ce11466aea21f69b7f6e13bb31
Steven Deobald <steven@deobald.ca>
2021-04-10 18:00:45 -0600

    pali word TXT ingestion writes to db

--- src/kosa/mobile/today/pali_word/txt_job.clj
+++ src/kosa/mobile/today/pali_word/txt_job.clj
@@ -1,6 +1,9 @@
 (ns kosa.mobile.today.pali-word.txt-job
   (:require [clojure.java.io :as io]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.tools.logging :as log]
+            [kosa.mobile.today.pali-word.db :as db])
+  (:import [java.net URI]))
 
 (defn split-pali-words [txt]
   (str/split txt #"~"))
@@ -11,8 +14,8 @@
        vec))
 
 (defn ->doc [lang entry]
-  {:pali (first entry)
-   :translations [[lang (second entry)]]})
+  {:pali-word/pali (first entry)
+   :pali-word/translations [[lang (second entry)]]})
 
 (defn parse [txt lang]
   (->> (split-pali-words txt)
@@ -20,5 +23,23 @@
        (map shred)
        (map (partial ->doc lang))))
 
+#_(defn db-insert [pali-word]
+  (let [existing (db/q :pali-word/original-pali
+                       (:pali-word/original-pali pali-word))]
+    (log/info "Pali Word RSS: attempting insert")
+    (when (= 0 (count existing))
+      (db/save! (merge {:pali-word/bookmarkable true
+                        :pali-word/shareable true}
+                       pali-word)))))
+
+(defn db-insert [pali-word]
+  (log/info "Pali Word TXT: attempting insert")
+  (db/save! (merge {:pali-word/bookmarkable true
+                    :pali-word/shareable true
+                    :pali-word/original-pali (:pali-word/pali pali-word)
+                    :pali-word/original-url (URI. "")}
+                   pali-word)))
+
 (defn ingest [f lang]
-  (parse (slurp f) lang))
+  (doseq [word (parse (slurp f) lang)]
+    (db-insert word)))
--- test/kosa/mobile/today/pali_word/txt_job_test.clj
+++ test/kosa/mobile/today/pali_word/txt_job_test.clj
@@ -1,15 +1,37 @@
 (ns kosa.mobile.today.pali-word.txt-job-test
   (:require [kosa.mobile.today.pali-word.txt-job :as sut]
+            [kosa.mobile.today.pali-word.db :as db]
             [kosa.fixtures.file-fixtures :as file-fixtures]
+            [kuti.fixtures.record-fixtures :as record-fixtures]
+            [kuti.fixtures.time-fixtures :as time-fixtures]
             [clojure.test :refer :all]))
+
+(use-fixtures :once
+  time-fixtures/freeze-clock-1995
+  record-fixtures/force-destroy-db
+  record-fixtures/force-migrate-db
+  record-fixtures/force-start-db)
 
 (deftest parsing-txt-file
   (testing "parses out text without whitespace or separators"
+    (let [f (file-fixtures/file "pali_word_raw.txt")
+          txt (slurp f)]
+      (is (= [{:pali-word/pali "vimutti"
+               :pali-word/translations [["en" "freedom, release, deliverance, emancipation, liberation"]]}
+              {:pali-word/pali "kataññū"
+               :pali-word/translations [["en" "kata + ññū = what is done + knowing, acknowledging what has been done (to, for one), grateful"]]}
+              {:pali-word/pali "tarati"
+               :pali-word/translations [["en" "to cross [a river], to surmount, overcome [the great flood of life, desire, ignorance], to get to the other side, to cross over, as in crossing the ocean of suffering"]]}]
+             (sut/parse txt "en"))))))
+
+(deftest ingesting-txt-file
+  (testing "inserts entries into db"
     (let [f (file-fixtures/file "pali_word_raw.txt")]
-      (is (= [{:pali "vimutti"
-               :translations [["en" "freedom, release, deliverance, emancipation, liberation"]]}
-              {:pali "kataññū"
-               :translations [["en" "kata + ññū = what is done + knowing, acknowledging what has been done (to, for one), grateful"]]}
-              {:pali "tarati"
-               :translations [["en" "to cross [a river], to surmount, overcome [the great flood of life, desire, ignorance], to get to the other side, to cross over, as in crossing the ocean of suffering"]]}]
-             (sut/parse (slurp f) "en"))))))
+      (sut/ingest f "en")
+      (is (= 3 (count (db/list))))))
+
+  (testing "TODO: ignore identical entities")
+
+  (testing "TODO: merge additional languages if merged is not identical")
+
+  (testing "TODO: figure out scheduling (override publish on `save!`?)"))
