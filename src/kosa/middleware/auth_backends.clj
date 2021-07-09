402519d3d869980c36085176dd81b75f7ee7e10e
Steven Deobald <steven@deobald.ca>
2021-07-08 21:50:27 -0600

    Removing debugging statements

--- src/kosa/middleware/auth_backends.clj
+++ src/kosa/middleware/auth_backends.clj
@@ -1,17 +1,13 @@
 (ns kosa.middleware.auth-backends
-  (:require [buddy.auth.protocols :as proto]
-            [kuti.support.debugging :refer [dbg]]))
+  (:require [buddy.auth.protocols :as proto]))
 
 (defn permissive
   [& [{:keys [realm authfn]}]]
   (reify
     proto/IAuthentication
     (-parse [_ request]
-      (dbg "#### parsing")
-      (dbg "always allow? (permissive)" (authfn request {:authdata :ignored}))
       (if (authfn request {:authdata :ignored})
         nil ;; weirdly, `nil` escapes the backend chain, so this is what we want
         :truthy))
     (-authenticate [_ request data]
-      (dbg "authenticate permissive, return nil")
       nil)))
