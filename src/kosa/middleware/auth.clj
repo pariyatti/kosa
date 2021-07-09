efa89c322996bd696ad997a8142a707051121be3
Steven Deobald <steven@deobald.ca>
2021-07-08 19:09:26 -0600

    Removing hard-coded password, upgrading to Crux 1.17.1
    - Crux 1.17.1 moves the deps to Maven Central (from Clojars). That
      fix got mixed in with the Buddy stuff.
    - Fixed middleware: all middleware has to be in reitit-style and we
      should keep it symmetrical (flat) rather than conj'ing it
    - Added buddy-hashers to validate password -- let's just use the
      default algo

--- src/kosa/middleware/auth.clj
+++ src/kosa/middleware/auth.clj
@@ -1,26 +1,50 @@
 (ns kosa.middleware.auth
   (:require [buddy.auth :refer [authenticated? throw-unauthorized]]
             [buddy.auth.backends :as backends]
+            [buddy.hashers :as hashers]
             [buddy.auth.middleware :refer [wrap-authentication
-                                           wrap-authorization]]
-            ))
+                                           wrap-authorization]]))
+
+;; NOTE: `derive` uses a random salt by default and you will not
+;;       get this same hash again if you recreate it from the password. -sd
+(def avs-password-hash "bcrypt+sha512$84f97d76ed0d9d5fce692136778af80b$12$ac37445c969fedad3642d5e2c414aa86cb6160b5eac50a63")
 
 (defn- auth-fn [request authdata]
   (let [username (:username authdata)
         password (:password authdata)]
-    (if (= password "shakyamuni")
+    (if (and
+         (= "admin" username)
+         (:valid (hashers/verify password
+                                 avs-password-hash)))
       username
       nil)))
 
-(def- backend (backends/basic {:realm "kosa" :authfn auth-fn}))
+(def backend (backends/basic {:realm "kosa" :authfn auth-fn}))
 
-(defn- basic-auth-middleware [handler]
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; public, reitit-style middleware wrappers:
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn wrap-wrap-authentication [handler]
+  (wrap-authentication handler backend))
+
+(def authentication-middleware
+  {:name ::authentication
+   :wrap wrap-wrap-authentication})
+
+(defn wrap-wrap-authorization [handler]
+  (wrap-authorization handler backend))
+
+(def authorization-middleware
+  {:name ::authorization
+   :wrap wrap-wrap-authorization})
+
+(defn basic-auth-middleware [handler]
   (fn [request]
     (if (authenticated? request)
       (handler request)
       (throw-unauthorized {:message "Not authorized"}))))
 
-(def middleware-list
-  [[wrap-authentication backend]
-   [wrap-authorization backend]
-   basic-auth-middleware])
+(def basic-http-auth-middleware
+  {:name ::basic-http-auth
+   :wrap basic-auth-middleware})
