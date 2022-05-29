37a94198cd25065cf79ce0d80bbf4ce02e64982e
Tanmay Balwa <tanmay.balwa@gmail.com>
2022-05-29 20:32:06 +0530

    WIP: Add python script to handle backups

---
+++ ops/scripts/backups/data_backup.py
@@ -0,0 +1,74 @@
+import boto3
+import requests
+import os
+import subprocess
+import sys
+import logging
+
+from discord import Webhook, RequestsWebhookAdapter
+
+log_path = "/var/log/kosa"
+file_name = "kosa-backups"
+logging.basicConfig(format="%(asctime)s %(funcName)s() [%(levelname)-5.5s]  %(message)s")
+file_handler = logging.FileHandler("{0}/{1}.log".format(log_path, file_name))
+log_formatter = logging.Formatter("%(asctime)s %(funcName)s() [%(levelname)-5.5s]  %(message)s")
+file_handler.setFormatter(log_formatter)
+logging.getLogger().addHandler(file_handler)
+
+def is_root():
+    return os.geteuid() == 0
+
+
+def discord_send_message(message):
+    """Send a message to pariyatti discord alerts channel
+
+    :param message: text to send to the channel
+    :type message: string
+    """
+    discord_url = os.environ["DISCORD_WEBHOOK_URL"]
+    webhook = Webhook.from_url(url=discord_url, adapter=RequestsWebhookAdapter())
+    webhook.send(message)
+
+
+def is_service_running(service_name="kosa-app.service"):
+    try:
+        cmd = ["systemctl", "status", service_name]
+        completed = subprocess.run(cmd, capture_output=True, text=True, check=True)
+    except subprocess.CalledProcessError as err:
+        logging.error("ERROR:", err)
+    else:
+        for line in completed.stdout.splitlines():
+            if "Active:" in line:
+                if "(running)" in line:
+                    print("{0} service is running".format(service_name))
+                    return True
+        return False
+
+
+def stop_service(service_name="kosa-app.service"):
+    if not is_root():
+        logging.error("This function requires super user privileges")
+        sys.exit(1)
+    try:
+        cmd = ["systemctl", "stop", service_name]
+        completed = subprocess.run(cmd, capture_output=True, text=True, check=True)
+    except subprocess.CalledProcessError as err:
+        logging.error("ERROR:", err)
+        discord_send_message(
+            "stop_service command failed to run! Please check the logs"
+        )
+
+
+def start_service(service_name="kosa-app.service"):
+    if not is_root():
+        logging.error("This function requires super user privileges")
+        sys.exit(1)
+    try:
+        cmd = ["systemctl", "start", service_name]
+        completed = subprocess.run(cmd, capture_output=True, text=True, check=True)
+    except subprocess.CalledProcessError as err:
+        logging.error("ERROR:", err)
+        discord_send_message(
+            "Kosa backend down! start_service command failed to run, please check the logs"
+        )
+
