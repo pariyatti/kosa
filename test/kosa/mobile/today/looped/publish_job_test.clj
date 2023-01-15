9ccf9f910ca6112d0e9f8b7d406147ef2bd8e3bf
Steven Deobald <steven@deobald.ca>
2023-01-14 23:24:11 -0600

    Fixing CI

--- test/kosa/mobile/today/looped/publish_job_test.clj
+++ test/kosa/mobile/today/looped/publish_job_test.clj
@@ -4,12 +4,25 @@
             [kuti.support.time :as time]
             [tick.alpha.api :as t]))
 
-(deftest publish-time-is-always-7am-pst-same-day
-  ;; 15:11 UTC is 07:11 PST
-  (testing "at the start of the day UTC, pretend to publish at 7am PST"
-    (is (= (t/instant "2012-06-15T15:11:02Z")
-           (sut/publish-time (t/instant "2012-06-15T00:00:01Z")))))
+(deftype StubPublisher []
+  sut/Publisher
+  (type [_] :stub)
+  (offset [_] "00:11:02")
+  (main-key [_] :stub/nothing)
+  (published-at-key [_] :stub/published-at)
+  (looped-list [_] [])
+  (looped-find [_ idx] [])
+  (entity-find [_ card] [])
+  (save! [_ card] nil))
 
-  (testing "at the end of the day UTC, pretend to publish at 7am PST"
-    (is (= (t/instant "2012-06-15T15:11:02Z")
-           (sut/publish-time (t/instant "2012-06-15T23:59:59Z"))))))
+(deftest publish-time-is-always-7am-pst-same-day
+  ;; PST is UTC+08:00 for our purposes
+  ;; TODO: rather than publishing in PST, as the email and RSS scripts do,
+  ;;       should we just publish in UTC directly? it seems less confusing.
+  (testing "at the start of the day UTC, pretend to publish at `offset` in PST"
+    (is (= (t/instant "2012-06-15T08:11:02Z")
+           (sut/publish-time (StubPublisher.) (t/instant "2012-06-15T00:00:01Z")))))
+
+  (testing "at the end of the day UTC, pretend to publish at `offset` in PST"
+    (is (= (t/instant "2012-06-15T08:11:02Z")
+           (sut/publish-time (StubPublisher.) (t/instant "2012-06-15T23:59:59Z"))))))
